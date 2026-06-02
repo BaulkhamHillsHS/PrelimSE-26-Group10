bdb1fd7d215dca1fe24a924f25a1530f2405ddf7
gavelhj <tgvbhjj2@gmail.com>
2026-06-02 12:53:35 +1000

    made it so you can viewpages and created skeleton for parent class "StandardPage" and basic UI for loginpage

--- main.py
+++ main.py
@@ -12,40 +12,66 @@ class FontSize:
     Text = 11
     """11"""
 
-class SettingsPage(ctk.CTkFrame):
-    def __init__(self):
-        super().__init__()
+class StandardPage(ctk.CTkFrame):
+    """
+    Parent class for pages that can be accessed when in a profile
+    """
+    def __init__(self, master, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
     
     def _build_ui(self):
         pass
 
-class VideoPage(ctk.CTkFrame):
+class SettingsPage(StandardPage):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+    def _build_ui(self):
+        pass
+
+class VideoPage(StandardPage):
     """
     Screen to display when selecting a movie/show
     """
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     
     def _build_ui(self):
         pass
 
 class LoginPage(ctk.CTkFrame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.grid_rowconfigure(0,weight=1)
+        self.grid_columnconfigure(0,weight=1)
+        self._build_ui()
     
     def _build_ui(self):
-        pass
+        self.login_frame = ctk.CTkFrame(self)
+        self.login_frame.grid(row=0, column=0, ipadx=100, ipady=0)
+        
+        self.text = ctk.CTkLabel(self.login_frame, text="Login Page")
+        self.text.pack(padx=0,pady=30)
+        
+        self.email_entry = ctk.CTkEntry(self.login_frame, placeholder_text="Enter your email")
+        self.email_entry.pack(padx=0,pady=20)
+        
+        self.password_entry = ctk.CTkEntry(self.login_frame, placeholder_text="Enter your password")
+        self.password_entry.pack(padx=0,pady=20)
+        
+        self.login_button = ctk.CTkButton(self.login_frame, text="Login")
+        self.login_button.pack(padx=0,pady=30)
 
 class SignUpPage(ctk.CTkFrame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     
     def _build_ui(self):
         pass
 
 class PaymentPlanPage(ctk.CTkFrame):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
     
     def _build_ui(self):
         pass
@@ -54,14 +80,30 @@ class StreamingApp(ctk.CTk):
     Title = "App"
     def __init__(self):
         super().__init__()
-        self.pages = []
-        self.currentpage = None
+        self.pages = {}
+        self.currentpage: ctk.CTkFrame = None
         self.account = None
         self.profile = None
+        
+        self.grid_rowconfigure(0,weight=1)
+        self.grid_columnconfigure(0,weight=1)
     
-    def _change_page(self):
-        pass
+    def _change_page(self, newpage):
+        if self.currentpage:
+            self.currentpage.grid_forget()
+        self.currentpage = self.pages[newpage]
+        self.currentpage.grid(row=0, column=0, sticky="nesw")
+    
+    def _test_page(self, page):
+        """
+        See what a page looks like (for testing only)
+        """
+        if self.currentpage:
+           self.currentpage.grid_forget()
+        self.currentpage = page
+        self.currentpage.grid(row=0, column=0, sticky="nesw")
         
 if __name__ == "__main__":
     app = StreamingApp()
+    app._test_page(LoginPage(app))
     app.mainloop()
