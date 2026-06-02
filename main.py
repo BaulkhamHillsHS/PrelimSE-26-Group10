eebcb9fea2b9996d6971343da41dfd79715f6e10
gavelhj <tgvbhjj2@gmail.com>
2026-06-02 12:09:17 +1000

    added (unfinished) classes for each page in the app

--- main.py
+++ main.py
@@ -12,21 +12,42 @@ class FontSize:
     Text = 11
     """11"""
 
-class VideoFrame(ctk.CTkFrame):
+class SettingsPage(ctk.CTkFrame):
+    pass
+
+class VideoPage(ctk.CTkFrame):
     """
-    UI component to display videos (movies and tv shows)
+    Screen to display when selecting a movie/show
     """
     def __init__(self):
         super().__init__()
     
-    def build_ui(self):
+    def _build_ui(self):
         pass
 
+class LoginPage(ctk.CTkFrame):
+    def __init__():
+        pass
+    
+    def _build_ui():
+        pass
+
+class SignUpPage(ctk.CTkFrame):
+    pass
+
+class PaymentPlanPage(ctk.CTkFrame):
+    pass
+
 class StreamingApp(ctk.CTk):
+    Title = "App"
     def __init__(self):
         super().__init__()
+        self.pages = []
+        self.currentpage = None
+        self.account = None
+        self.profile = None
     
-    def build_ui(self):
+    def _change_page(self):
         pass
         
 if __name__ == "__main__":
