3d0b6b7c24b7f06676aa2d3520caf2849a790478
gavelhj <tgvbhjj2@gmail.com>
2026-06-18 20:59:16 +1000

    added a method to delete profiles and moved csv-related functions to another file named csvmodule.py

--- accountmodule.py
+++ accountmodule.py
@@ -85,6 +85,53 @@ def edit_row(filename, fields_to_search: list, data: dict, newdata: dict):
     writer = csv.DictWriter(f, fieldnames)
     writer.writeheader()
     writer.writerows(filedata)
+    f.close()
+
+def delete_row(filename, fields_to_search: list, data: dict):
+    if filename == "profiles.csv":
+        f = open("profiles.csv", mode="r+", newline="")
+        fieldnames = ["accountemail", "profilename", "age", "watchhistory"]
+    elif filename == "accounts.csv":
+        f = open("accounts.csv", mode="r+", newline="")
+        fieldnames = ["accountname", "email", "password", "plan", "profiles"]
+    else:
+        print("Invalid file name")
+        return False
+    
+    reader = csv.DictReader(f)
+    filedata = []
+    for row in reader: #copy the file into a list
+        found_row = True
+        for field in fields_to_search:
+            
+            if row[field] != str(data[field]):
+                found_row = False
+                
+        if not found_row: #append non-deleted row
+            filedata.append(row)
+    
+    writer = csv.DictWriter(f, fieldnames)
+    writer.writeheader()
+    writer.writerows(filedata)
+    f.close()
+
+def append_row(filename, data: dict):
+    if filename == "profiles.csv":
+        f = open("profiles.csv", mode="a", newline="")
+        fieldnames = ["accountemail", "profilename", "age", "watchhistory"]
+    elif filename == "accounts.csv":
+        f = open("accounts.csv", mode="a", newline="")
+        fieldnames = ["accountname", "email", "password", "plan", "profiles"]
+    else:
+        print("Invalid file name")
+        return False
+    
+    writer = csv.DictWriter(f, fieldnames)
+    writer.writerow(data)
+    
+    f.close()
+
+delete_row("profiles.csv", ["accountemail"], {"accountemail": "tgvbhjj2@gmail.com"})
 
 class Profile:
     def __init__(self, account, name):
